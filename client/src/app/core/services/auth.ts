0d084b663e10143dc4a892ca68d0f903e13fa196
muhamad-amin-wafi_tmberhad <muhamad.amin.wafi@vtcholding.com>
2026-01-11 05:45:05 +0800

    feature (client/home): add environments

--- client/src/app/core/services/auth.ts
+++ client/src/app/core/services/auth.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 import { tap } from 'rxjs'; 
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = `http://localhost:3000/api/auth`;
+  private apiUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient) {}
 
