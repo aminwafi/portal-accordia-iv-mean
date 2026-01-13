29bc7d94672fd21d83e290f471798c016d821907
muhamad-amin-wafi_tmberhad <muhamad.amin.wafi@vtcholding.com>
2026-01-13 12:22:43 +0800

    feature (server/admin/item): separate admin and user logic

--- server/routes/item/index.js
+++ server/routes/item/index.js
@@ -9,7 +9,5 @@ router.use(authenticate);
 router.get('/', itemCtrl.listItems);
 router.get('/:id', itemCtrl.getItem);
 router.post('/', itemCtrl.createItem);
-router.put('/:id', itemCtrl.updateItem);
-router.delete('/:id', itemCtrl.deleteItem);
 
 module.exports = router;
